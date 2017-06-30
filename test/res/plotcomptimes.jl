81d0e8a856b5052f9ee94fc838d55381997575f5
Krzysztof Domino <kdomino@iitis.pl>
2017-06-30 12:03:15 +0200

    correction of plotcomptimes colours and scalling

--- test/res/plotcomptimes.jl
+++ test/res/plotcomptimes.jl
@@ -2,6 +2,7 @@
 
 using PyCall
 @pyimport matplotlib as mpl
+mpl.rc("text", usetex=true)
 mpl.use("Agg")
 using PyPlot
 using JLD
@@ -19,16 +20,24 @@ function singleplot(filename::String, name::String, compare::String = "")
   x = d["x"]
   t = d["t"]
   m = d["m"]
-  mpl.rc("text", usetex=true)
-  mpl.rc("font", family="serif", size = 8)
-  fig, ax = subplots(figsize = (3, 2.3))
+  mpl.rc("font", family="serif", size = 7)
+  fig, ax = subplots(figsize = (2.5, 2.))
+  col = ["red", "blue", "black", "green", "yellow", "orange"]
   for i in 1:size(comptimes, 2)
     tt = t[i]
-    ax[:plot](d[x], comptimes[:,i], "--x", label= "t = $tt")
+    ax[:plot](d[x], comptimes[:,i], ":s", label= "t = $tt", color = col[i], markersize=2.5, linewidth = 1)
   end
-  PyPlot.ylabel(ylab, labelpad = -1)
-  PyPlot.xlabel(x, labelpad = -3)
-  ax[:legend](fontsize = 7, loc = 2, ncol = 1)
+  PyPlot.ylabel(ylab, labelpad = -1.8)
+  PyPlot.xlabel(x, labelpad = -2)
+  if maximum(comptimes) > 10
+    f = matplotlib[:ticker][:ScalarFormatter]()
+    f[:set_powerlimits]((-3, 2))
+  else
+    f = matplotlib[:ticker][:FormatStrFormatter]("%.1f")
+  end
+  ax[:yaxis][:set_major_formatter](f)
+  ax[:legend](fontsize = 6, loc = 2, ncol = 1)
+  subplots_adjust(bottom = 0.12,top=0.92)
   fig[:savefig](name*filename*".eps")
 end
 
@@ -52,7 +61,7 @@ end
 function main(args)
   s = ArgParseSettings("description")
   @add_arg_table s begin
-    "--file", "-f"
+    "file"
     help = "the file name"
     arg_type = String
   end
