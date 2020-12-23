54ad525f8a7fe9eb81b0d8c35b4c1a3f345f5e34
kdomino <kdomino@iitis.pl>
2020-12-23 10:36:34 +0100

    add nightly

--- src/Cumulants.jl
+++ src/Cumulants.jl
@@ -5,6 +5,9 @@ module Cumulants
   using Distributed
   import SymmetricTensors: pyramidindices, ind2range, sizetest, getblockunsafe
   import Distributions: moment
+  if VERSION >= v"1.3"
+   using CompilerSupportLibraries_jll
+ end
 
   #calculates moments and cumulants using block structures (SymmetricTensors)
   include("cumulant.jl")
